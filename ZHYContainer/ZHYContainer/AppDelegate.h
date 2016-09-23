949429fa285c44b17635af77d5f62ff268576625
Henry <zhy289044312@gmail.com>
2016-09-23 23:06:53 +0800

    [create] 创建工程

---
+++ ZHYContainer/ZHYContainer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ZHYContainer
+//
+//  Created by Henry on 2016/9/23.
+//  Copyright © 2016年 Henry. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ZHYContainer/ZHYContainer/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ZHYContainer
+//
+//  Created by Henry on 2016/9/23.
+//  Copyright © 2016年 Henry. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
